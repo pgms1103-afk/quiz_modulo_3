dd85b7daed8c81f636073465e9be2b74a3791304
pgms1103-afk <pgms1103@gmail.com>
2026-04-22 10:32:35 -0500

    BACK

---
+++ quizmodulo2/quizmodulo2/src/main/java/co/edu/unbosque/quizmodulo2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.quizmodulo2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Quizmodulo2Application.class);
+	}
+
+}
